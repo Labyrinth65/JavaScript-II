50bb33465c4ada0be052e05392fe69a8d94e7d55
Ang Xu <angxu1234@gmail.com>
2019-05-07 14:32:03 -0500

    complete

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,13 +1,18 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+const a = 0
+function addOne (num) {
+  const a = 1;
+  return num + a;
+}
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
+  let count = 0
+  return function () {count += 1; return count}
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
